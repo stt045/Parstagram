fc51150a3d77569e8d5b251d5fee1963f8d61cf0
Steven Tran <steventraaan@gmail.com>
2019-04-07 02:31:52 -0700

    configured auto-layout for feedView

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Steven Tran on 4/7/19.
+//  Copyright © 2019 Steven Tran. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
